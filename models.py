11237c650e6a25eb11b37701c735d7ad946272fe
NKPaliwal21 <nkp2102@gmail.com>
2025-07-16 22:36:50 +0530

    Update models.py
    Change select & insert to support railway(online db), so remove ivrtest.

--- models.py
+++ models.py
@@ -7,13 +7,14 @@ cursor = conn.cursor()
 
 # Fetch a user by username
 def get_user_by_username(username):
-    cursor.execute("SELECT * FROM ivrtest.users WHERE username = %s", (username,))
+#    cursor.execute("SELECT * FROM ivrtest.users WHERE username = %s", (username,))
+    cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+
     return cursor.fetchone()
 
 # Insert a new user
 def create_user(username, password):
     cursor.execute(
-        "INSERT INTO ivrtest.users (username, password) VALUES (%s, %s)",
-        (username, password)
-    )
+#        "INSERT INTO ivrtest.users (username, password) VALUES (%s, %s)",(username, password))
+        "INSERT INTO ivrtest.users (username, password) VALUES (%s, %s)",(username, password))        
     conn.commit()
