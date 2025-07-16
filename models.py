4ccf089ca88f8ae4e6cd1cf78004d8aae48478b7
NKPaliwal21 <nkp2102@gmail.com>
2025-07-16 23:17:15 +0530

    Update models.py
    added try and exception.

--- models.py
+++ models.py
@@ -2,19 +2,45 @@ import psycopg2
 from config import Config
 
 # Connect to the PostgreSQL database
-conn = psycopg2.connect(Config.DB_URL)
-cursor = conn.cursor()
+# conn = psycopg2.connect(Config.DB_URL)
+# cursor = conn.cursor()
 
 # Fetch a user by username
-def get_user_by_username(username):
-#    cursor.execute("SELECT * FROM ivrtest.users WHERE username = %s", (username,))
-    cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
 
-    return cursor.fetchone()
+# def get_user_by_username(username):
+#    cursor.execute("SELECT * FROM ivrtest.users WHERE username = %s", (username,))
+#    cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+#    return cursor.fetchone()
+
+def get_user_by_username(username):
+    try:
+        conn = psycopg2.connect(Config.DB_URL)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+        user = cursor.fetchone()
+        conn.close()
+        return user
+    except Exception as e:
+        print(f"DB Error in get_user_by_username: {e}")
+        return None
+
+
+
 
 # Insert a new user
-def create_user(username, password):
-    cursor.execute(
+#def create_user(username, password):
+#    cursor.execute(
 #        "INSERT INTO ivrtest.users (username, password) VALUES (%s, %s)",(username, password))
-        "INSERT INTO ivrtest.users (username, password) VALUES (%s, %s)",(username, password))        
-    conn.commit()
+#        "INSERT INTO ivrtest.users (username, password) VALUES (%s, %s)",(username, password))        
+#    conn.commit()
+
+def create_user(username):
+    try:
+        conn = psycopg2.connect(Config.DB_URL)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO ivrtest.users (username, password) VALUES (%s, %s)",(username, password))
+        conn.commit()
+        conn.close()        
+    except Exception as e:
+        print(f"DB Error in create_user: {e}")
+        return None
